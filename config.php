360ad4dcdd5ce40e012f27b1fbb2616c3d5ac307
Jesse <83730324+dev-jesse@users.noreply.github.com>
2022-09-05 12:21:03 -0600

    added cleardb vars

--- config.php
+++ config.php
@@ -1,16 +1,24 @@
 <?php 
 
-$server = "us-cdbr-east-06.cleardb.net";
-$user = "b325bf00362f81";
-$pass = "f98f7673";
-$database = "fractal_login_system";
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-$conn = mysqli_connect($server, $user, $pass, $database);
+// $server = "us-cdbr-east-06.cleardb.net";
+// $user = "b325bf00362f81";
+// $pass = "f98f7673";
+// $database = "fractal_login_system";
 
-if (!$conn) {
-    die("<script>alert('Connection Failed.')</script>");
-}
+// $conn = mysqli_connect($server, $user, $pass, $database);
 
-
+// if (!$conn) {
+//     die("<script>alert('Connection Failed.')</script>");
+// }
 
 ?>
