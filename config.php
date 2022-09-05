e6f32f139dbbe2d22fe02c16bb29c885ec4caf1c
Jesse <83730324+dev-jesse@users.noreply.github.com>
2022-09-05 12:08:56 -0600

    Added clearDB:

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php 
 
-$server = "localhost";
-$user = "root";
-$pass = "";
+$server = "us-cdbr-east-06.cleardb.net";
+$user = "b325bf00362f81";
+$pass = "f98f7673";
 $database = "fractal_login_system";
 
 $conn = mysqli_connect($server, $user, $pass, $database);
