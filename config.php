096fc5c5f497091aadf676931fc5861bd6ac8001
Jesse <83730324+dev-jesse@users.noreply.github.com>
2022-09-05 12:24:50 -0600

    offical cleardb

--- config.php
+++ config.php
@@ -1,14 +1,13 @@
 <?php 
 
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
 
 // $server = "us-cdbr-east-06.cleardb.net";
 // $user = "b325bf00362f81";
