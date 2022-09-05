4a0a0e28a599b871c929b5e367bf855a4e84ba79
Jesse <83730324+dev-jesse@users.noreply.github.com>
2022-09-05 13:09:30 -0600

    minor issue with variables

--- config.php
+++ config.php
@@ -3,11 +3,11 @@
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
+$user = $url["user"];
+$pass = $url["pass"];
 $db = substr($url["path"], 1);
 
-$conn = new mysqli($server, $username, $password, $db);
+$conn = new mysqli($server, $user, $pass, $db);
 
 
 // Localhost:
